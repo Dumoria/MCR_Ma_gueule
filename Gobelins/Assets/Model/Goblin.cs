c2a864469a0366c649139537d5dab6354fe691a5
Cloud <benjamin.thomas@heig-vd.ch>
2018-06-08 15:24:05 +0200

    script

---
+++ Gobelins/Assets/Model/Goblin.cs
@@ -0,0 +1,20 @@
+public class Goblin{
+    int stress;
+    int salaire;
+    bool greviste;
+    int id;
+    string emploi;
+    Goblin* collegue;
+    Goblin* superieur;
+    Goblin* employe;
+
+    public Goblin(){
+         
+    }
+
+    public Goblin(){
+
+
+    }
+
+}
