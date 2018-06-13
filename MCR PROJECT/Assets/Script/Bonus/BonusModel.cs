0ea4ab168e3bacddbc81b7354b7d002807c4c52c
olliens <oussama.lagha@heig-vd.ch>
2018-06-13 03:07:06 +0200

    ajout bonus classes

---
+++ MCR PROJECT/Assets/Script/Bonus/BonusModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MODEL{
+	public class Bonus
+	{
+
+	    private int cost;
+
+		public Bonus(){}
+		public setCost(int cost){
+			this.cost=cost;
+		}
+	    public int getCost()
+	    {
+	        return cost;
+	    }
+	}
+}
---
+++ MCR PROJECT/Assets/Script/Bonus/PoudreDePhenix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoudreDePhenix : BonusModel {
+	public static int niveauPoudre=0;
+public PoudreDePhenix:base(){}
+public int getNiveauPoudre(){
+	return niveauPoudre;
+}
+public void setNiveauPoudre(int niveau){
+	this.niveauPoudre=niveau;
+}
+}
